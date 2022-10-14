db3cef638dd87eaed6c3036230af01c51c53d4f2
bright <brightosagiri@gmail.com>
2022-10-14 13:13:38 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - reverse numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'Z'; i >= 'A'; i--)
+	{
+		putchar("%c", i);
+	}
+	putchar('\n');
+	return (0);
+}
